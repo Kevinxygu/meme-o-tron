833e94c15d937d97066f75550f955b2d1c1ff675
Ryan Haraki <54621020+RyanHaraki@users.noreply.github.com>
2025-02-09 14:19:19 -0800

    move images to public folder

--- memeotron/src/app/page.tsx
+++ memeotron/src/app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       meme-o-tron
+      <Image
+        src="/images/trollFace.jpg"
+        alt="spiderman"
+        width={200}
+        height={200}
+      />
     </div>
   );
 }
